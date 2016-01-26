c6c56ad6018d3bafee679e20ba556f059e7a7639
jnylen <me@jnylen.nu>
2016-01-26 02:12:15 +0000

    Fix minus and some kelvin shit

--- plugins/unit.rb
+++ plugins/unit.rb
@@ -4,7 +4,7 @@ class Unit
   include Cinch::Plugin
   include Cinch::Formatting
 
-  match /^([\d\.,]{1,})\s([a-zA-Z]{1,})\sto\s([a-zA-Z]{1,})$/, :use_prefix => false
+  match /^([\d\.,\-]{1,})\s([a-zA-Z]{1,})\sto\s([a-zA-Z]{1,})$/, :use_prefix => false
 
   def execute(m, amount, unit, to_unit)
     old_unit = unit
@@ -21,7 +21,7 @@ class Unit
     begin
       newamount = RubyUnits::Unit.new(amount + " " + unit).convert_to(to_unit)
       debug "#{amount} #{unit} to #{to_unit} is #{newamount.to_s}"
-      m.reply Format(:bold, "[unit]") + " #{amount} #{old_unit} is #{newamount.to_s.gsub("tempF", "F").gsub("tempC", "C")}"
+      m.reply Format(:bold, "[unit]") + " #{amount} #{old_unit} is #{newamount.to_s.gsub("tempF", "F").gsub("tempC", "C").gsub("tempK", "K").gsub("tempR", "R")}"
     rescue ArgumentError
       m.reply Format(:bold, "[unit]") + " Not a valid unit."
     rescue
